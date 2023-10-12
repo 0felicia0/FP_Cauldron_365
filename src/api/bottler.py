949de4180fcd27e578264a71813ee06056671a1d
Felicia <fpatel2903@gmail.com>
2023-10-12 14:38:14 -0700

    did some wacky stuff

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -29,7 +29,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
             #connection.execute(sqlalchemy.text("UPDATE potions SET num_potions = potions.num_potions - :quantity WHERE potions.red_ml = :potion_type[0] AND potions.green_ml = :potion_type[1] AND potions.blue_ml = :potion_type[2] AND potions.dark = :potion_type[3]"))
 
             result = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
-
+            result_list = list(result)
             # sum of potions calculated with python syntax
             potions_to_add = 0
 
@@ -40,7 +40,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
             for potion in potions_delivered:
                 print(potion)
 
-                for row in result:
+                for row in result_list:
                     red_ml = row.red_ml
                     green_ml = row.green_ml
                     blue_ml = row.blue_ml
