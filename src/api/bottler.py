8ad63f59c518a58a649afb1a411a20ab7a1ab58b
Felicia <fpatel2903@gmail.com>
2023-10-12 14:07:33 -0700

    did some wacky stuff

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -50,9 +50,9 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
 
                         potions_to_add += potion.quantity
 
-                        red_ml_used -= row.red_ml
-                        green_ml_used -= row.green_ml
-                        blue_ml_used -= row.blue_ml
+                        red_ml_used += row.red_ml
+                        green_ml_used += row.green_ml
+                        blue_ml_used += row.blue_ml
                         
                         connection.execute(sqlalchemy.text("UPDATE potions SET num_potions = num_potions + :potions_to_add WHERE red_ml = :red_ml AND green_ml = :green_ml AND blue_ml = :blue_ml AND dark = :dark"), {"potions_to_add": potions_to_add, "potion_type": potion.potion_type, "red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark": dark})
 
