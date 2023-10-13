9cd04e60bf628ee1906b749d38d3302c0f624853
Felicia <fpatel2903@gmail.com>
2023-10-13 14:01:46 -0700

    did some wacky stuff

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -31,38 +31,40 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
             result = connection.execute(sqlalchemy.text("SELECT * FROM potions"))
             result_list = list(result)
             # sum of potions calculated with python syntax
-
-            red_ml_used = 0
-            green_ml_used = 0
-            blue_ml_used = 0
+            red_ml_used = sum(potion.quantity * potion.potion_type[0] for potion in potions_delivered)
+            green_ml_used = sum(potion.quantity * potion.potion_type[1] for potion in potions_delivered)
+            blue_ml_used = sum(potion.quantity * potion.potion_type[2] for potion in potions_delivered)
+            dark_used = sum(potion.quantity * potion.potion_type[3] for potion in potions_delivered)
 
             for potion in potions_delivered:
                 print(potion)
-                potions_to_add = 0
-                for row in result_list:
-                    red_ml = row.red_ml
-                    green_ml = row.green_ml
-                    blue_ml = row.blue_ml
-                    dark = row.dark
 
-                    row_potion_mix = [red_ml, green_ml, blue_ml, dark]
+                connection.execute(sqlalchemy.text("UPDATE potions SET num_potions = num_potions + :quantity WHERE red_ml = :red_ml AND green_ml = :green_ml AND blue_ml = :blue_ml AND dark = :dark"), {"quantity": potion.quantity, "red_ml": potion.potion_type[0], "green_ml": potion.potion_type[1], "blue_ml": potion.potion_type[2], "dark": potion.potion_type[3]})
 
-                    print("potion.potion_type: ", potion.potion_type)
-                    print("row mix: ", row_potion_mix)
+                # potions_to_add = 0
+                # for row in result_list:
+                #     red_ml = row.red_ml
+                #     green_ml = row.green_ml
+                #     blue_ml = row.blue_ml
+                #     dark = row.dark
 
-                    if potion.potion_type == row_potion_mix:
-                        print("current inventory of sku: ", row.sku, "inventory: ", row.num_potions)
+                #     row_potion_mix = [red_ml, green_ml, blue_ml, dark]
 
-                        potions_to_add += potion.quantity
+                #     print("potion.potion_type: ", potion.potion_type)
+                #     print("row mix: ", row_potion_mix)
 
-                        red_ml_used += row.red_ml * potion.quantity
-                        green_ml_used += row.green_ml * potion.quantity
-                        blue_ml_used += row.blue_ml * potion.quantity
+                #     if potion.potion_type == row_potion_mix:
+                #         print("current inventory of sku: ", row.sku, "inventory: ", row.num_potions)
+
+                #         potions_to_add += potion.quantity
                         
-                        connection.execute(sqlalchemy.text("UPDATE potions SET num_potions = num_potions + :potions_to_add WHERE red_ml = :red_ml AND green_ml = :green_ml AND blue_ml = :blue_ml AND dark = :dark"), {"potions_to_add": potions_to_add, "potion_type": potion.potion_type, "red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark": dark})
+                #         connection.execute(sqlalchemy.text("UPDATE potions SET num_potions = num_potions + :potions_to_add WHERE red_ml = :red_ml AND green_ml = :green_ml AND blue_ml = :blue_ml AND dark = :dark"), {"potions_to_add": potions_to_add, "potion_type": potion.potion_type, "red_ml": red_ml, "green_ml": green_ml, "blue_ml": blue_ml, "dark": dark})
 
-                        print("resulting potion number: sku: ", row.sku, "potions_to_add: ", potions_to_add)
-            print("red used: ", red_ml_used, " green used: ", green_ml_used, " blue used: ", blue_ml_used)
+                #         print("resulting potion number: sku: ", row.sku, "potions_to_add: ", potions_to_add)
+
+
+            print("red used: ", red_ml_used, " green used: ", green_ml_used, " blue used: ", blue_ml_used, "dark used: ", dark_used)
+
             connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_ml = num_red_ml - :red_ml_used, num_green_ml = num_green_ml - :green_ml_used, num_blue_ml = num_blue_ml - :blue_ml_used"), {"red_ml_used": red_ml_used, "green_ml_used": green_ml_used, "blue_ml_used": blue_ml_used})
 
 
