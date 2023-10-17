08e3c3929571c07dec4de6d68b0b81ef98f9758c
Felicia <fpatel2903@gmail.com>
2023-10-16 21:35:36 -0700

    starting assignment 4

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -24,11 +24,11 @@ def get_catalog():
             # grab all rows with available potions
             # what i want to do: get all potions that have quantity greater than zero
             # need info from potions table where the SUM(change) > 0 and potion.potion_id = potion_ledger.potion_id
-            result = connection.execute(sqlalchemy.text("""SELECT sku, name, price, type 
-                                                        FROM potions 
-                                                        JOIN potion_ledger ON potions.potion_id = potion_ledger.potion_id
-                                                        GROUP BY potions.id
-                                                        HAVING SUM(potion_ledger.change > 0)"""))
+            result = connection.execute(sqlalchemy.text("""SELECT p.sku, p.name, p.price, p.type 
+                                                        FROM potions p
+                                                        JOIN potion_ledger pl ON p.id = pl.potion_id
+                                                        GROUP BY p.sku, p.name, p.price, p.type
+                                                        HAVING SUM(pl.change) > 0"""))
             
             # result = connection.execute(sqlalchemy.text("""SELECT sku, name, num_potions, price, type 
             #                                             FROM potions 
