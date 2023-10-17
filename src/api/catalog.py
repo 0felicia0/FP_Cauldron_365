5fe94242d3de915f38a4af880f4896bed035e1ae
Felicia <fpatel2903@gmail.com>
2023-10-16 21:30:32 -0700

    starting assignment 4

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,25 +14,39 @@ def get_catalog():
     catalog = []
 
     with db.engine.begin() as connection:
+            """
+            SELECT sku, name, price, type 
+            FROM potions 
+            JOIN potion_ledger ON potions.potion_id = potion_ledger.potion_id
+            GROUP BY potions.id
+            HAVING SUM(potion_ledger.change > 0)
+            """
             # grab all rows with available potions
             # what i want to do: get all potions that have quantity greater than zero
             # need info from potions table where the SUM(change) > 0 and potion.potion_id = potion_ledger.potion_id
-            # result = connection.execute(sqlalchemy.text("SELECT sku, name, price, type FROM potions JOIN potion_id ON potion_ledger.potion_id GROUP BY potions.potion_id HAVING SUM(potion_ledger.change) > 0"))
-            result = connection.execute(sqlalchemy.text("SELECT sku, name, num_potions, price, type FROM potions WHERE num_potions > 0"))
+            result = connection.execute(sqlalchemy.text("""SELECT sku, name, price, type 
+                                                        FROM potions 
+                                                        JOIN potion_ledger ON potions.potion_id = potion_ledger.potion_id
+                                                        GROUP BY potions.id
+                                                        HAVING SUM(potion_ledger.change > 0)"""))
+            
+            # result = connection.execute(sqlalchemy.text("""SELECT sku, name, num_potions, price, type 
+            #                                             FROM potions 
+            #                                             WHERE num_potions > 0"""))
 
             for row in result:
-                #   print(row)
-                 potion = {
-                     "sku": row.sku,
-                     "name": row.name,
-                     "quantity": row.num_potions,
-                     "price": row.price,
-                     "potion_type": row.type,
-                 }
+                   print(row)
+    #              potion = {
+    #                  "sku": row.sku,
+    #                  "name": row.name,
+    #                  "quantity": row.num_potions,
+    #                  "price": row.price,
+    #                  "potion_type": row.type,
+    #              }
 
-                 catalog.append(potion)
+    #              catalog.append(potion)
     
-    return catalog           
+    # return catalog           
 
     
 
