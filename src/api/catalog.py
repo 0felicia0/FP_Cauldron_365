7cd7ebb8d9ca7676b919c9c743ced3f41484225d
Felicia <fpatel2903@gmail.com>
2023-10-16 21:57:24 -0700

    im losing my mind

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,13 +14,7 @@ def get_catalog():
     catalog = []
 
     with db.engine.begin() as connection:
-            """
-            SELECT sku, name, price, type 
-            FROM potions 
-            JOIN potion_ledger ON potions.potion_id = potion_ledger.potion_id
-            GROUP BY potions.id
-            HAVING SUM(potion_ledger.change > 0)
-            """
+            
             # grab all rows with available potions
             # what i want to do: get all potions that have quantity greater than zero
             # need info from potions table where the SUM(change) > 0 and potion.potion_id = potion_ledger.potion_id
@@ -35,18 +29,18 @@ def get_catalog():
             #                                             WHERE num_potions > 0"""))
 
             for row in result:
-                   print(row)
-    #              potion = {
-    #                  "sku": row.sku,
-    #                  "name": row.name,
-    #                  "quantity": row.num_potions,
-    #                  "price": row.price,
-    #                  "potion_type": row.type,
-    #              }
+                #print(row)
+                potion = {
+                      "sku": row.sku,
+                      "name": row.name,
+                      "quantity": row.num_potions,
+                      "price": row.price,
+                      "potion_type": row.type,
+                }
 
-    #              catalog.append(potion)
-    
-    # return catalog           
+                catalog.append(potion)
+    print(catalog)
+    return catalog           
 
     
 
