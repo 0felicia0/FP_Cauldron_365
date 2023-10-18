a567168517163a9ae30c119cfed385591a730fd8
Felicia <fpatel2903@gmail.com>
2023-10-18 10:01:45 -0700

    adjusted barrel and bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -101,7 +101,7 @@ def get_bottle_plan():
             for potion in potions_result:
                 bottled = 0
 
-                while(bottled < 5 and red_ml >= potion.type[0] and green_ml >= potion.type[1] and blue_ml >= potion.type[2] and dark_ml >= potion.type[3]):
+                while(bottled < 10 and red_ml >= potion.type[0] and green_ml >= potion.type[1] and blue_ml >= potion.type[2] and dark_ml >= potion.type[3]):
                     bottled += 1
                     # subtract from available ml in inventory
                     red_ml -= potion.type[0]
