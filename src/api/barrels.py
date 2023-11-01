f8cd7b5a9f11f23c14cf89f59600da77c4664412
Felicia <fpatel2903@gmail.com>
2023-10-31 22:13:36 -0700

    working with barrels

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -122,9 +122,9 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     # green_to_buy = ml_to_buy // 3 
     # blue_to_buy = ml_to_buy // 3 
 
-    red_to_buy = 2000
-    green_to_buy = 2000
-    blue_to_buy = 2000
+    red_to_buy = 3000
+    green_to_buy = 3000
+    blue_to_buy = 3000
     
 
     print("total_potions: ", total_potions, " potions to make: ", potions_to_make, " ml_to_buy: ", ml_to_buy, " ml_per_color: ", red_to_buy)
