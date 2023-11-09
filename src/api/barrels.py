09e6ee764c8b579eb207dd8d2f1858cd29f4c53d
Felicia <fpatel2903@gmail.com>
2023-11-09 12:53:42 -0800

    double barrel purchase

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -121,9 +121,9 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     ml_to_buy = potions_to_make * 100
 
-    red_to_buy = ml_to_buy // 3 
-    green_to_buy = ml_to_buy // 3 
-    blue_to_buy = ml_to_buy // 3 
+    red_to_buy = (ml_to_buy // 3) * 2 
+    green_to_buy = (ml_to_buy // 3) * 2 
+    blue_to_buy = (ml_to_buy // 3) * 2 
 
     # red_to_buy = 3000
     # green_to_buy = 3000
