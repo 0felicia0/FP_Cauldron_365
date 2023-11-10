00d5ff020bc264fafb0222076a897da900d82352
Felicia <fpatel2903@gmail.com>
2023-11-10 11:15:53 -0800

    buying

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -125,9 +125,9 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     green_to_buy = (ml_to_buy // 3) * 2 
     blue_to_buy = (ml_to_buy // 3) * 2 
 
-    #red_to_buy = 8000
-    #green_to_buy = 8000
-    # blue_to_buy = 3000
+    red_to_buy = 8000
+    green_to_buy = 8000
+    blue_to_buy = 8000
     
 
     print("total_potions: ", total_potions, " potions to make: ", potions_to_make, " ml_to_buy: ", ml_to_buy, " ml_per_color: ", red_to_buy)
