0b7b40bf2a9516cff46c0c77da3bebd27ba08a1f
Felicia <fpatel2903@gmail.com>
2023-11-13 12:29:02 -0800

    anotha one

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -125,15 +125,15 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     green_to_buy = (ml_to_buy // 3) 
     blue_to_buy = (ml_to_buy // 3) 
 
-    # red_to_buy = 6000
-    # green_to_buy = 6000
-    # blue_to_buy = 6000
+    red_to_buy = 20000
+    green_to_buy = 10000
+    blue_to_buy = 20000
     
 
     print("total_potions: ", total_potions, " potions to make: ", potions_to_make, " ml_to_buy: ", ml_to_buy, " ml_per_color: ", red_to_buy)
 
-    if total_potions < 300:
-        for barrel in wholesale_catalog:
+    #if total_potions < 300:
+    for barrel in wholesale_catalog:
             print(barrel)
             barrels_to_purchase = 0
 
